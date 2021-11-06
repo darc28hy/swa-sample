ed7a47f8d42f2ed146dd82e357f202147c726825
h-yoshida <yoshida@upward.jp>
2021-11-06 14:05:45 +0900

    lint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,30 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-
-import { Hero } from '../components/Hero'
+import { Link as ChakraLink, Text, Code, List, ListIcon, ListItem } from '@chakra-ui/react'
 import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { Footer } from '../components/Footer'
+import { Hero } from '../components/Hero'
+import { Main } from '../components/Main'
 
 const Index = () => (
-  <Container height="100vh">
+  <Container height='100vh'>
     <Hero />
     <Main>
       <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
+        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> + <Code>typescript</Code>.
       </Text>
 
       <List spacing={3} my={0}>
         <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
+          <ListIcon as={CheckCircleIcon} color='green.500' />
+          <ChakraLink isExternal href='https://chakra-ui.com' flexGrow={1} mr={2}>
             Chakra UI <LinkIcon />
           </ChakraLink>
         </ListItem>
         <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+          <ListIcon as={CheckCircleIcon} color='green.500' />
+          <ChakraLink isExternal href='https://nextjs.org' flexGrow={1} mr={2}>
             Next.js <LinkIcon />
           </ChakraLink>
         </ListItem>
