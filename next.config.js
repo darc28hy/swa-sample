c114165b4f9a780ccd66503f2bcfa73cf8eb37dc
h-yoshida <yoshida@upward.jp>
2021-11-06 15:33:37 +0900

    next.config.js に認証ルート追加

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       '/': { page: '/' },
+      '/login': { page: '/login' },
+      '/logout': { page: '/logout' },
+      '/.auth/login/aad': { page: '/.auth/login/aad' },
+      '/.auth/logout': { page: '/.auth/logout' },
       '/private': { page: '/private' },
     }
   },
